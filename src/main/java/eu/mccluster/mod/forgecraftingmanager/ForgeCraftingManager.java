dfeb402ae2884786742cf0068d7bd7c539579778
I9hdkill <jonathanfriedrich@live.de>
2017-06-22 00:46:52 +0200

    Allow Team armor

--- src/main/java/eu/mccluster/mod/forgecraftingmanager/ForgeCraftingManager.java
+++ src/main/java/eu/mccluster/mod/forgecraftingmanager/ForgeCraftingManager.java
@@ -38,9 +38,9 @@ public class ForgeCraftingManager
         forbiddenrecipes.add("pixelmon:thunder_stone_shovel");
         forbiddenrecipes.add("pixelmon:thunder_stone_hoe");
         forbiddenrecipes.add("pixelmon:thunder_stone_sword");
-        forbiddenrecipes.add("pixelmon:thunder_stone_helm");
-        forbiddenrecipes.add("pixelmon:thunder_stone_plate");
-        forbiddenrecipes.add("pixelmon:thunder_stone_legs");
+        //forbiddenrecipes.add("pixelmon:thunder_stone_helm");
+        //forbiddenrecipes.add("pixelmon:thunder_stone_plate");
+        //forbiddenrecipes.add("pixelmon:thunder_stone_legs");
         forbiddenrecipes.add("pixelmon:thunder_stone_boots");
         
         forbiddenrecipes.add("pixelmon:fire_stone_hammer");
@@ -50,30 +50,30 @@ public class ForgeCraftingManager
         forbiddenrecipes.add("pixelmon:fire_stone_hoe");
         forbiddenrecipes.add("pixelmon:fire_stone_sword");
         
-        forbiddenrecipes.add("pixelmon:sapphire_helm");
-        forbiddenrecipes.add("pixelmon:sapphire_plate");
-        forbiddenrecipes.add("pixelmon:sapphire_legs");
-        forbiddenrecipes.add("pixelmon:sapphire_boots");
+        //forbiddenrecipes.add("pixelmon:sapphire_helm");
+        //forbiddenrecipes.add("pixelmon:sapphire_plate");
+        //forbiddenrecipes.add("pixelmon:sapphire_legs");
+        //forbiddenrecipes.add("pixelmon:sapphire_boots");
         
-        forbiddenrecipes.add("pixelmon:galactic_helm");
-        forbiddenrecipes.add("pixelmon:galactic_plate");
-        forbiddenrecipes.add("pixelmon:galactic_legs");
-        forbiddenrecipes.add("pixelmon:galactic_boots");
+        //forbiddenrecipes.add("pixelmon:galactic_helm");
+        //forbiddenrecipes.add("pixelmon:galactic_plate");
+        //forbiddenrecipes.add("pixelmon:galactic_legs");
+        //forbiddenrecipes.add("pixelmon:galactic_boots");
         
-        forbiddenrecipes.add("pixelmon:ruby_helm");
-        forbiddenrecipes.add("pixelmon:ruby_plate");
-        forbiddenrecipes.add("pixelmon:ruby_legs");
-        forbiddenrecipes.add("pixelmon:ruby_boots");
+        //forbiddenrecipes.add("pixelmon:ruby_helm");
+        //forbiddenrecipes.add("pixelmon:ruby_plate");
+        //forbiddenrecipes.add("pixelmon:ruby_legs");
+        //forbiddenrecipes.add("pixelmon:ruby_boots");
         
-        forbiddenrecipes.add("pixelmon:neo_plasma_helm");
-        forbiddenrecipes.add("pixelmon:neo_plasma_plate");
-        forbiddenrecipes.add("pixelmon:neo_plasma_legs");
-        forbiddenrecipes.add("pixelmon:neo_plasma_boots");
+        //forbiddenrecipes.add("pixelmon:neo_plasma_helm");
+        //forbiddenrecipes.add("pixelmon:neo_plasma_plate");
+        //forbiddenrecipes.add("pixelmon:neo_plasma_legs");
+        //forbiddenrecipes.add("pixelmon:neo_plasma_boots");
         
-        forbiddenrecipes.add("pixelmon:plasma_helm");
-        forbiddenrecipes.add("pixelmon:plasma_plate");
-        forbiddenrecipes.add("pixelmon:plasma_legs");
-        forbiddenrecipes.add("pixelmon:plasma_boots");
+        //forbiddenrecipes.add("pixelmon:plasma_helm");
+        //forbiddenrecipes.add("pixelmon:plasma_plate");
+        //forbiddenrecipes.add("pixelmon:plasma_legs");
+        //forbiddenrecipes.add("pixelmon:plasma_boots");
         
         forbiddenrecipes.add("pixelmon:rocket_helm");
         forbiddenrecipes.add("pixelmon:rocket_plate");
